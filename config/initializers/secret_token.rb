5f10c1a024fc55ac6b1e1fc6ebf4f01c3049d7d4
ogoldberg <ogoldberg@gmail.com>
2012-08-07 14:48:14 -0400

    the actual initial commit of all the code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liad::Application.config.secret_token = '16281bf60721421306ef5f28e205d5819724a34a4abece13438e9b39b14f6fed3d8be238a2bf315a5aac9737d4b285883abd236896e3ff0ffb3bd2ae311700e6'
